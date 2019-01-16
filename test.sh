63eb3e44cafe104af4375bfad82adb63b35cb9cc
Luke Siler <lsiler@pryoninc.com>
2019-01-16 13:49:06 -0800

    make executable and add simple test

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker run lukesiler/gcloud-sdk-plus yq
+docker run lukesiler/gcloud-sdk-plus jq
+docker run lukesiler/gcloud-sdk-plus go version
+docker run lukesiler/gcloud-sdk-plus kubectl
+docker run lukesiler/gcloud-sdk-plus gcloud
