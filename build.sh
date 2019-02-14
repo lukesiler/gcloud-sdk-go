c3ae8ba868f4886754d78d8246ceca5eed8b361d
Luke Siler <lsiler@pryoninc.com>
2019-02-14 14:52:41 -0800

    fix args

--- build.sh
+++ build.sh
@@ -5,12 +5,12 @@ REPO=$(basename "$(git rev-parse --show-toplevel)")
 IMAGE_NAME=gcloud-sdk-plus
 IMAGE_TAG=latest
 
-docker build -t ${IMAGE_NAME}:${IMAGE_TAG} . \
+docker build -t ${IMAGE_NAME}:${IMAGE_TAG} \
             --build-arg IMAGE_SRC="docker.io/lukesiler/${IMAGE_NAME}:${IMAGE_TAG}" \
             --build-arg BUILT_BY="$(whoami)" \
             --build-arg BUILT_AT="$(date -u '+%F %T.%3N')" \
- #           --build-arg SEMVER=${DOCKER_TAG} \
             --build-arg GIT_REPO="${REPO}" \
             --build-arg GIT_BRANCH="${BRANCH}" \
             --build-arg GIT_COMMIT="$(git rev-parse HEAD)" \
-            --build-arg GIT_STATUS="$(git status -s)"
+            --build-arg GIT_STATUS="$(git status -s)" .
+#            --build-arg SEMVER=${DOCKER_TAG} \
