eed739b168b689e69f7931f24549a0bdb64a94d4
Luke Siler <lsiler@pryoninc.com>
2019-02-15 10:00:47 -0800

    use semver tag to optimize caching that latest tag does not allow for

--- push.sh
+++ push.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-IMAGE_NAME=gcloud-sdk-plus
-IMAGE_TAG=latest
+IMAGE_NAME=$(yq r data.yaml data.image-name)
+SEMVER=$(yq r data.yaml data.semver)
 
 docker login
-docker push lukesiler/${IMAGE_NAME}:${IMAGE_TAG}
+docker push ${IMAGE_NAME}:v${SEMVER}
+docker tag ${IMAGE_NAME}:v${SEMVER} ${IMAGE_NAME}:latest
