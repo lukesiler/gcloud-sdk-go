3d4069f37164cb6e890629e77e6eb700e0cf24d2
Luke Siler <lsiler@pryoninc.com>
2019-06-12 15:12:12 -0700

    add test command that was accidentally dropped

--- test.sh
+++ test.sh
@@ -9,3 +9,4 @@ docker run --rm ${IMAGE_NAME}:v${SEMVER} go version
 docker run --rm ${IMAGE_NAME}:v${SEMVER} kubectl version
 docker run --rm ${IMAGE_NAME}:v${SEMVER} gcloud version
 docker run --rm ${IMAGE_NAME}:v${SEMVER} ginkgo version
+docker run --rm ${IMAGE_NAME}:v${SEMVER} gcloud beta emulators datastore start --no-store-on-disk --project fake
